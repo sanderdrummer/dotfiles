2e382d9061a2cee28fd507fb7679c7106627c1ba
Tobias Pickel <tobias.pickel@moia.io>
2024-01-19 08:57:08 +0100

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,17 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install node
+brew install neovim
+
+brew install bruno
+brew install --cask amethyst
+brew install --cask karabiner-elements
+brew install yqrashawn/goku/goku
+brew install --cask doll
+brew install lazygit
+brew install ripgrep
+brew install bottom
+
+
+
+npm install tree-sitter-cli
